369ea036142df77cbf86f6c413090efa4f75e113
CarPer2002 <jhordict20@gmail.com>
2025-09-23 11:24:20 -0500

    feat(i18n): integrate material design, and apply new theme setup.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,18 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import {provideHttpClient, withFetch} from '@angular/common/http';
+import {provideTranslateService} from '@ngx-translate/core';
+import {provideTranslateHttpLoader} from '@ngx-translate/http-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({prefix: './i18n/', suffix: '.json'}),
+      fallbackLang: 'en'
+    })
   ]
 };
